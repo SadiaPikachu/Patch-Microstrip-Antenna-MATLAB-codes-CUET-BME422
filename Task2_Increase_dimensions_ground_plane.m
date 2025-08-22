99c12acfa22102a61600dd13d9c68ff193afe9bf
Sadia Tabassum <sadiast2001st@gmail.com>
2025-08-22 18:14:26 +0600

    Task2_Increase_dimensions_ground_plane.m

---
+++ Task2_Increase_dimensions_ground_plane.m
@@ -0,0 +1,30 @@
+%%Task 2(Increase dimensions of the ground plane by 50%)
+%% Create the Patch Antenna
+p1 = patchMicrostrip;
+%%  Physical Dimensions of the Patch 
+p1.Length = 40e-3;
+p1.Width = 30e-3;
+p1.Height = 1.57e-3;
+%% Ground Plane Size
+p1.GroundPlaneLength = (80e-3)*1.5;
+p1.GroundPlaneWidth = (60e-3)*1.5;
+%%  Feed Point position
+p1.FeedOffset = [5.5e-3 0];
+%% Substrate Material
+p1.Substrate = dielectric('Name', 'material1', 'EpsilonR', 2.33);
+pb1 = pcbStack(p1);
+%% Feed Details
+pb1.FeedDiameter = sqrt(2)*1e-3;
+pb1.FeedViaModel = "square"
+figure
+show(pb1)
+title("Patch Antenna with Thin Substrate")
+%% meshing
+figure
+mesh(pb1,MaxEdgeLength=0.01,MinEdgeLength=0.003)
+%% Input Impedance 
+figure
+impedance(pb1,linspace(2.2e9,2.5e9,5))
+%% Radiation Pattern
+figure
+pattern(pb1,2.37e9)
