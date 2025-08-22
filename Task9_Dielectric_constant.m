18a32a746a0a60dc6540b4d9528d4fb9cabb3e62
Sadia Tabassum <sadiast2001st@gmail.com>
2025-08-22 18:53:24 +0600

    Task9_Dielectric_constant.m

---
+++ Task9_Dielectric_constant.m
@@ -0,0 +1,31 @@
+%% Task 9(Try different value of Dielectric constant of substrate material (1,5 and 10))
+n case Dielectric constant = 1:
+%% Create the Patch Antenna
+p1 = patchMicrostrip;
+%%  Physical Dimensions of the Patch 
+p1.Length = 40e-3;
+p1.Width = 30e-3;
+p1.Height = 1.57e-3;
+%% Ground Plane Size
+p1.GroundPlaneLength = 80e-3;
+p1.GroundPlaneWidth = 60e-3;
+%%  Feed Point position
+p1.FeedOffset = [5.5e-3 0];
+%% Substrate Material
+p1.Substrate = dielectric('Name', 'material1', 'EpsilonR', 1); % change 1 to 5, 10 based on task requirement
+pb1 = pcbStack(p1);
+%% Feed Details
+pb1.FeedDiameter = sqrt(2)*1e-3;
+pb1.FeedViaModel = "square"
+figure
+show(pb1)
+title("Patch Antenna with Thin Substrate")
+%% meshing
+figure
+mesh(pb1,MaxEdgeLength=0.01,MinEdgeLength=0.003)
+%% Input Impedance 
+figure
+impedance(pb1,linspace(2.2e9,2.5e9,5))
+%% Radiation Pattern
+figure
+pattern(pb1,2.37e9)
